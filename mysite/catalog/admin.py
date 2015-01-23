3cc47615dd1bee6c2934999d563ab6801fa5a74a
VMarshinskii <marshinskii@gmail.com>
2015-01-23 23:47:14 +0500

    Create admin Product

--- mysite/catalog/admin.py
+++ mysite/catalog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from catalog.models import Product
+from django.shortcuts import render_to_response
+
 
 # Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    def changelist_view(self, request, extra_context=None):
+        vars = {'categories': "dsjfskdf"}
+        html = render_to_response('admin/result_content_list.html', vars).content
+        mass = {'result_content': html}
+        return super(ProductAdmin, self).changelist_view(request, extra_context=mass)
+
+    
+admin.site.register(Product, ProductAdmin)
