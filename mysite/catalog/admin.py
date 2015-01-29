4c18a1e14646d2a6c7a809274016b89c0b1df576
VMarshinskii <marshinskii@gmail.com>
2015-01-29 09:33:26 +0500

    ChangeForm

--- mysite/catalog/admin.py
+++ mysite/catalog/admin.py
@@ -6,11 +6,11 @@ from django.shortcuts import render_to_response
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
 
-    def changelist_view(self, request, extra_context=None):
+    def changeform_view(self, request, extra_context=None):
         vars = {'categories': "dsjfskdf"}
         html = render_to_response('admin/result_content_list.html', vars).content
         mass = {'result_content': html}
-        return super(ProductAdmin, self).changelist_view(request, extra_context=mass)
+        return super(ProductAdmin, self).changeform_view(request, extra_context=mass)
 
 
 admin.site.register(Product, ProductAdmin)
