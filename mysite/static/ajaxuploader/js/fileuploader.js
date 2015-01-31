2aa876cc08b86a51ded18251f5aaf1965b27ced2
VMarshinskii <marshinskii@gmail.com>
2015-01-31 14:46:26 +0500

    Update

--- mysite/static/ajaxuploader/js/fileuploader.js
+++ mysite/static/ajaxuploader/js/fileuploader.js
@@ -9,7 +9,33 @@
  *
  * Source:
  * https://github.com/GoodCloud/django-ajax-uploader/blob/master/ajaxuploader/static/ajaxuploader/js/fileuploader.js
- */    
+ */
+
+function get_cookie(name)
+{
+    var cookie_name = name + "=";
+    var cookie_length = document.cookie.length;
+    var cookie_begin = 0;
+    while (cookie_begin < cookie_length)
+    {
+        var value_begin = cookie_begin + cookie_name.length;
+        if (document.cookie.substring(cookie_begin, value_begin) == cookie_name)
+        {
+            var value_end = document.cookie.indexOf (";", value_begin);
+            if (value_end == -1)
+            {
+                value_end = cookie_length;
+            }
+            return unescape(document.cookie.substring(value_begin, value_end));
+        }
+        cookie_begin = document.cookie.indexOf(" ", cookie_begin) + 1;
+        if (cookie_begin == 0)
+        {
+        break;
+        }
+    }
+    return null;
+}
 
 //
 // Helper functions
@@ -1229,7 +1255,7 @@ qq.extend(qq.UploadHandlerXhr.prototype, {
      */    
     _upload: function(id, params){
         var file = this._files[id],
-            name = params['name_new'] + '.' + this.getName(id).split('.')[1],
+            name = get_cookie("filename") + '.' + this.getName(id).split('.')[1],
             size = this.getSize(id);
                 
         this._loaded[id] = 0;
