84ce4abf748d4f21fe28fd2478d7736c8339e785
VMarshinskii <marshinskii@gmail.com>
2015-02-06 01:03:10 +0500

    Update

--- mysite/banners/admin.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.contrib import admin
-from banners.models import RightBanner, Slider
-
-# Register your models here.
-admin.register(RightBanner)
-admin.register(Slider)
