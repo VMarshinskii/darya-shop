040ec31e99023ed2704e46d14cdc1b1803f0bb9d
VMarshinskii <marshinskii@gmail.com>
2015-02-05 21:02:34 +0500

    Update

--- mysite/catalog/models.py
+++ mysite/catalog/models.py
@@ -9,8 +9,7 @@ class Category(models.Model):
     url = models.CharField("Url", max_length=200, blank=True)
     description = models.CharField("Description", max_length=200, blank=True)
     keywords = models.CharField("Ключевые слова", max_length=200, blank=True)
-    step = models.IntegerField("Вложенность", blank=True, editable=False, default=0)
-
+    step = models.IntegerField("Вложенность", blank=True, editable=False)
 
     def __unicode__(self):
         return self.title
