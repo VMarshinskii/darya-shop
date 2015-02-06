3a1622aad49609cee5aab4e7cec61473e2cdf0a1
VMarshinskii <marshinskii@gmail.com>
2015-02-06 12:42:16 +0500

    Update

--- mysite/static/js/admin.js
+++ mysite/static/js/admin.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     var path = $(".file-upload").children("a").attr("href");
     $(".file-upload").children("a").attr("href", '/' + path);
-    $(".file-upload").children("a").html('<img width="140px" style="margin:10px 0 10px 0" src="/' + path + '"');
+    $(".file-upload").children("a").html('fsdgfsdfgsdfgsdfgsd');
 });
