4837d134d369ada933ae401a576105b6edb1e974
VMarshinskii <marshinskii@gmail.com>
2015-02-09 17:03:56 +0500

    Update

--- mysite/pages/templatetags/catalog_menu.py
+++ mysite/pages/templatetags/catalog_menu.py
@@ -1,6 +1,14 @@
 from django import template
+from catalog.models import Category
 register = template.Library()
 
 @register.inclusion_tag('templatetags/catalog_menu.html')
 def catalog_menu():
-    return {'oll_pages': "dfdsfgsdfg"}
+    parents = Category.objects.filter(parent=None)
+    mass_categ = {}
+    for cat in parents:
+        mass_categ[cat] = {}
+        childrens = Category.objects.filter(parent=cat)
+        for child in childrens:
+            mass_categ[cat].append(child)
+    return {'mass_categ': mass_categ}
