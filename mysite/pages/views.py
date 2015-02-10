bf243ae4b14c649dc3cbdec963b842ef0d129f8d
VMarshinskii <marshinskii@gmail.com>
2015-02-10 23:41:32 +0500

    Update

--- mysite/pages/views.py
+++ mysite/pages/views.py
@@ -8,7 +8,7 @@ from pages.models import Page
 # Create your views here.
 def page_view(request, url="None"):
     try:
-        page = Page.objects.get(url="stranica")
+        page = Page.objects.get(url=url)
     except Page.DoesNotExist:
         raise Http404
     return render_to_response("page.html", {'page': page})
