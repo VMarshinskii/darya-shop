a9b32132f55a974fb32451a2e4be5d74314fbc18
VMarshinskii <marshinskii@gmail.com>
2015-02-11 22:00:26 +0500

    Update

--- mysite/pages/urls.py
+++ mysite/pages/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^page/(?P<url>w+/$)', 'pages.views.page_view'),
+    url(r'^(?P<url>\w+/$)', 'pages.views.page_view'),
 )
