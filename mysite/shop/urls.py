065b5a3d3ed0218c5a10cc8ba401e8ae12a78521
VMarshinskii <marshinskii@gmail.com>
2015-02-17 23:08:59 +0500

    Update

--- mysite/shop/urls.py
+++ mysite/shop/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^', 'shop.views.cart'),
-    url(r'^order', 'shop.views.order'),
+    url(r'^$', 'shop.views.cart'),
+    url(r'^order$', 'shop.views.order'),
 )
