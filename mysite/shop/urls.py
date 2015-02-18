538b08ffd7aa7221162c53e7869a25b6c04bb644
VMarshinskii <marshinskii@gmail.com>
2015-02-18 18:48:26 +0500

    Update

--- mysite/shop/urls.py
+++ mysite/shop/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^$', 'shop.views.cart'),
     url(r'^order/$', 'shop.views.order'),
-    url(r'^$add_in_cart/(?P<id>\d+)/$', 'shop.views.add_in_cart'),
+    url(r'^add_in_cart/(?P<id>\d+)/$', 'shop.views.add_in_cart'),
 )
