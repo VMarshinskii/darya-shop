02c5f004b6bb32b2d8dae0d50c5f06fc9645bd75
VMarshinskii <marshinskii@gmail.com>
2015-02-19 23:47:05 +0500

    Update

--- mysite/pages/templatetags/top_cart.py
+++ mysite/pages/templatetags/top_cart.py
@@ -3,22 +3,21 @@ from pages.models import Page
 register = template.Library()
 
 @register.inclusion_tag('templatetags/top_cart.html', takes_context=True)
-def top_cart():
+def top_cart(context):
     sum = 0
     count_val = 0
     request = context['request']
-    if "user_cart" in request.session:
-        user_key = request.COOKIES['filename']
-        try:
-            user_cart = UserCart.objects.get(user_key=user_key)
-            products = unserialize(user_cart.products)
-            for id, count in products:
-                try:
-                    pr = Product.objects.get(id=int(id))
-                    sum += pr.price * int(count)
-                    count_val += int(count)
-                except Product.DoesNotExist:
-                    pass
-        except UserCart.DoesNotExist:
-            pass
+    user_key = request.session['user_key']
+    try:
+        user_cart = UserCart.objects.get(user_key=user_key)
+        products = unserialize(user_cart.products)
+        for id, count in products:
+            try:
+                pr = Product.objects.get(id=int(id))
+                sum += pr.price * int(count)
+                count_val += int(count)
+            except Product.DoesNotExist:
+                pass
+    except UserCart.DoesNotExist:
+        pass
     return {'sum': sum, 'count': count_val}
