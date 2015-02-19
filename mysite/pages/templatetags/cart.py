a254336b99f853d62b673322dd27eaf7ba02844b
VMarshinskii <marshinskii@gmail.com>
2015-02-19 22:33:46 +0500

    Update

--- mysite/pages/templatetags/cart.py
+++ mysite/pages/templatetags/cart.py
@@ -3,11 +3,10 @@ from catalog.models import Product
 from shop.models import UserCart
 from shop.view import unserialize, serialize
 from django.http import Http404
-
 register = template.Library()
 
-@register.inclusion_tag('templatetags/cart_module.html')
-def cart_module():
+@register.inclusion_tag('templatetags/cart.html')
+def cart():
     sum = 0
     count_val = 0
     if "user_cart" in request.session:
