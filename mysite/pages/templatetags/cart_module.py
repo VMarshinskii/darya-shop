a1b35c7fdbac2658d7258a20a0a2c4b115b74104
VMarshinskii <marshinskii@gmail.com>
2015-02-19 22:23:29 +0500

    Update

--- mysite/pages/templatetags/cart_module.py
+++ mysite/pages/templatetags/cart_module.py
@@ -10,18 +10,4 @@ register = template.Library()
 def cart_module():
     sum = 0
     count_val = 0
-    if "user_cart" in request.session:
-        user_key = request.session["user_cart"]
-        try:
-            user_cart = UserCart.objects.get(user_key=user_key)
-            products = unserialize(user_cart.products)
-            for id, count in products:
-                try:
-                    pr = Product.objects.get(id=int(id))
-                    sum += pr.price * int(count)
-                    count_val += int(count)
-                except Product.DoesNotExist:
-                    pass
-        except UserCart.DoesNotExist:
-            pass
     return {'sum': sum, 'count': count_val}
