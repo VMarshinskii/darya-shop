27171f407beb2d19ba34ed426a67bf3bd949491a
VMarshinskii <marshinskii@gmail.com>
2015-02-19 23:29:04 +0500

    Update

--- mysite/pages/templatetags/top_cart.py
+++ mysite/pages/templatetags/top_cart.py
@@ -4,4 +4,20 @@ register = template.Library()
 
 @register.inclusion_tag('templatetags/top_cart.html')
 def top_cart():
-    return {'count': 0, 'sum': 0}
+    sum = 0
+    count_val = 0
+    if "user_cart" in request.session:
+        user_key = request.session["user_cart"]
+        try:
+            user_cart = UserCart.objects.get(user_key=user_key)
+            products = unserialize(user_cart.products)
+            for id, count in products:
+                try:
+                    pr = Product.objects.get(id=int(id))
+                    sum += pr.price * int(count)
+                    count_val += int(count)
+                except Product.DoesNotExist:
+                    pass
+        except UserCart.DoesNotExist:
+            pass
+    return {'sum': sum, 'count': count_val}
