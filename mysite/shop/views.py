c1a683766c009f3d35d49902119a45cd321e01f4
VMarshinskii <marshinskii@gmail.com>
2015-02-24 22:24:49 +0500

    Update

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -7,11 +7,10 @@ import random
 import string
 
 
-# Create your views here.
-def cart(request):
-    products = {}
-    sum_mass = {}
+def return_cart(request):
     sum = 0
+    count_all = 0
+    products = []
     if "user_cart" in request.session:
         user_key = request.session["user_cart"]
         try:
@@ -19,15 +18,41 @@ def cart(request):
             for product_id, count in unserialize(user_cart.products).items():
                 try:
                     pr = Product.objects.get(id=product_id)
-                    pr.price = (pr.price / 100) * (100 - pr.sale)
-                    pr.price_sum = pr.price * int(count)
-                    products[pr] = count
-                    sum += pr.price * int(count)
+                    pr.price_new = (pr.price / 100) * (100 - pr.sale)
+                    pr.price_sum_new = pr.price_new * int(count)
+                    pr.price_sum_old = pr.price * int(count)
+                    pr.count = int(count)
+                    products.append(pr)
+                    count_all += int(count)
+                    sum += pr.price_new * int(count)
                 except Product.DoesNotExist:
                     pass
         except UserCart.DoesNotExist:
             pass
-    return render_to_response("cart.html", {'products': products, 'sum_mass': sum_mass, 'sum': sum})
+    return {'count': count_all, 'sum': sum, 'products': products}
+
+
+def unserialize(str):
+    products = {}
+    if str == '':
+        return products
+    for i in str.split(";"):
+        if i != '':
+            mass_str = i.split(":")
+        products[int(mass_str[0])] = int(mass_str[1])
+    return products
+
+
+def serialize(products):
+    str_mass = []
+    for key, value in products.items():
+        str_mass.append(str(key) + ":" + str(value))
+    return ";".join(str_mass)
+
+
+def cart(request):
+    cart_mass = return_cart(request)
+    return render_to_response("cart.html", {'products': cart_mass['products'], 'sum': cart_mass['sum']})
 
 
 def order(request):
@@ -165,46 +190,3 @@ def cart_top_ajax(request):
         except UserCart.DoesNotExist:
             pass
     return render_to_response("cart_top_ajax.html", {'count': count_all, 'sum': sum})
-
-
-def return_cart(request):
-    sum = 0
-    count_all = 0
-    products = []
-    if "user_cart" in request.session:
-        user_key = request.session["user_cart"]
-        try:
-            user_cart = UserCart.objects.get(user_key=user_key)
-            for product_id, count in unserialize(user_cart.products).items():
-                try:
-                    pr = Product.objects.get(id=product_id)
-                    pr.price_new = (pr.price / 100) * (100 - pr.sale)
-                    pr.price_sum_new = pr.price_new * int(count)
-                    pr.price_sum_old = pr.price * int(count)
-                    pr.count = int(count)
-                    products.append(pr)
-                    count_all += int(count)
-                    sum += pr.price_new * int(count)
-                except Product.DoesNotExist:
-                    pass
-        except UserCart.DoesNotExist:
-            pass
-    return {'count': count_all, 'sum': sum, 'products': products}
-
-
-def unserialize(str):
-    products = {}
-    if str == '':
-        return products
-    for i in str.split(";"):
-        if i != '':
-            mass_str = i.split(":")
-        products[int(mass_str[0])] = int(mass_str[1])
-    return products
-
-
-def serialize(products):
-    str_mass = []
-    for key, value in products.items():
-        str_mass.append(str(key) + ":" + str(value))
-    return ";".join(str_mass)
