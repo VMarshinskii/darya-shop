9d10f6d20c696c538fd4731a2785158b9d247531
VMarshinskii <marshinskii@gmail.com>
2015-02-26 16:50:19 +0500

    Update

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -67,7 +67,7 @@ def order(request):
 
     if request.method == 'POST':
         form = OrderForm(request.POST)
-        form.type_delivery = TypeDelivery.objects.get(id=int(form.cleaned_data['type_delivery']))
+        form.type_delivery = TypeDelivery.objects.get(id=int(form.type_delivery))
         if form.is_valid():
             return render_to_response("order.html", args)
         else:
