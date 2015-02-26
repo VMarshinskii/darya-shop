eeffb0e36aba343c59942f36b1dbccfcaa0d3601
VMarshinskii <marshinskii@gmail.com>
2015-02-26 17:27:15 +0500

    Update

--- mysite/shop/forms.py
+++ mysite/shop/forms.py
@@ -6,3 +6,4 @@ from models import Order
 class OrderForm(ModelForm):
     class Meta:
         model = Order
+        fields = ['type_delivery', 'name', 'surname', 'mail', 'phone', 'region', 'city', 'index', 'address']
