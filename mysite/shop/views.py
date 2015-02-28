67d456c79ff69f74dd22c43e7f312fe562b6655b
VMarshinskii <marshinskii@gmail.com>
2015-02-28 16:06:35 +0500

    Update

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -116,7 +116,7 @@ def order(request):
                     args['form'] = form
                     #рендерим 3-ю форму
                     return render_to_response("order3.html", args)
-        args['form'] = OrderForm3
+        args['form'] = OrderForm3(request.POST)
         return render_to_response("order3.html", args)
 
     if request.method == 'POST':
