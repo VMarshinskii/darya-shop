c584c92a449893a911bed91dba7859b9f58660b5
VMarshinskii <marshinskii@gmail.com>
2015-03-01 16:47:03 +0500

    Update

--- mysite/shop/forms.py
+++ mysite/shop/forms.py
@@ -1,24 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from models import Order
-from django import forms
 
 
 class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = ['type_delivery', 'name', 'surname', 'mail', 'phone', 'region', 'city', 'index', 'address']
-
-
-class OrderForm2(forms.Form):
-    address_id = forms.CharField(max_length=200)
-    type_delivery = forms.CharField(max_length=200)
-    region = forms.CharField(max_length=200)
-    city = forms.CharField(max_length=200)
-    index = forms.CharField(max_length=200)
-    address = forms.CharField(max_length=200, widget=forms.Textarea)
-
-
-class OrderForm3(forms.Form):
-    address_id = forms.CharField(max_length=200)
-    type_delivery = forms.CharField(max_length=200)
+        widgets = {
+            'address': Textarea(),
+        }
