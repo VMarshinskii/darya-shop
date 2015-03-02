ddca870ce1adbbd102cda24cb1ba96e63bbf5824
VMarshinskii <marshinskii@gmail.com>
2015-03-02 13:46:59 +0500

    Update

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -53,22 +53,22 @@ def order(request):
         if request.POST:
             form = OrderForm(request.POST)
             if form.is_valid():
-                ord = create_order(request, request.user)
-                user = update_user(request) or request.user
+                ord = additions.create_order(request, request.user)
+                user = additions.update_user(request) or request.user
                 return render_to_response("order_thanks.html")
             else:
                 args['form'] = form
                 return render_to_response("order_registered.html", args)
-        model = get_model_order(request)
+        model = additions.get_model_order(request)
         args['form'] = OrderForm(instance=model)
         return render_to_response("order_registered.html", args)
 
     if request.POST:
         form = OrderForm(request.POST)
         if form.is_valid():
-            password = random_str(7)
-            user = create_user(request, password)
-            ord = create_order(request, user)
+            password = additions.random_str(7)
+            user = additions.create_user(request, password)
+            ord = additions.create_order(request, user)
             return render_to_response("order_thanks.html", {'ord': ord, 'password': password})
         else:
             args['form'] = form
@@ -85,9 +85,9 @@ def add_in_cart(request, id=-1):
         except UserCart.DoesNotExist:
             user_cart = UserCart()
             user_cart.user_key = user_key
-        products = unserialize(user_cart.products)
+        products = additions.unserialize(user_cart.products)
         products[int(id)] = products.get(int(id), 0) + 1
-        user_cart.products = serialize(products)
+        user_cart.products = additions.serialize(products)
         user_cart.save()
     else:
         user_cart = UserCart()
@@ -109,12 +109,12 @@ def del_in_cart(request, id=-1):
         except UserCart.DoesNotExist:
             user_cart = UserCart()
             user_cart.user_key = user_key
-        products = unserialize(user_cart.products)
+        products = additions.unserialize(user_cart.products)
         if int(id) in products and products[int(id)] > 0:
             products[int(id)] -= 1
             if products[int(id)] == 0:
                 products.pop(int(id))
-            user_cart.products = serialize(products)
+            user_cart.products = additions.serialize(products)
             user_cart.save()
     cart_mass = return_cart(request)
     return render_to_response("cart_ajax.html", {'products': cart_mass['products'], 'sum': cart_mass['sum']})
@@ -128,9 +128,9 @@ def remove_in_cart(request, id=-1):
         except UserCart.DoesNotExist:
             user_cart = UserCart()
             user_cart.user_key = user_key
-        products = unserialize(user_cart.products)
+        products = additions.unserialize(user_cart.products)
         products.pop(int(id))
-        user_cart.products = serialize(products)
+        user_cart.products = additions.serialize(products)
         user_cart.save()
     cart_mass = return_cart(request)
     return render_to_response("cart_ajax.html", {'products': cart_mass['products'], 'sum': cart_mass['sum']})
