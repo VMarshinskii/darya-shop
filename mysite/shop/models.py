f3c38930be19ce4bba5cd85d963929b4b418cc60
VMarshinskii <marshinskii@gmail.com>
2015-03-11 17:41:06 +0500

    Update

--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -33,7 +33,7 @@ class Order(models.Model):
     user = models.ForeignKey(User, verbose_name="Пользователь", blank=True, null=True)
     type_delivery = models.ForeignKey(TypeDelivery, verbose_name="Вариант доставки", null=True)
     products = models.CharField("Заказ", max_length=200)
-    status = models.CharField("Статус", max_length=1, choices=Order_Status)
+    status = models.CharField("Статус", max_length=200, choices=Order_Status)
     date_create = models.DateField(verbose_name="Дата создания заказа", auto_now_add=True)
     date_change = models.DateField(verbose_name="Дата редактирования заказа", auto_now=True)
     admin_comment = models.TextField(verbose_name="Комментарий администратора", blank=True)
