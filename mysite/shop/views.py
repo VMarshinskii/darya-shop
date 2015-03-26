c685361b223843d7e5c14a4d6dec5848b6d3d762
VMarshinskii <marshinskii@gmail.com>
2015-03-26 16:42:53 +0500

    Update

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -78,7 +78,7 @@ def order(request):
             if user:
                 ord = create_order(request, user)
                 #отправка на e-mail и sms
-                sms(user.phone, "DARYA-SHOP: Ваш заказ оформлен. Данные для входа в личный кабинет: " + user.phone + ", " + password)
+                sms(user.phone, u"DARYA-SHOP: Ваш заказ оформлен. Данные для входа в личный кабинет: " + user.phone + u", " + password)
                 return render_to_response("order_thanks.html", {'ord': ord, 'password': password})
             else:
                 args['error'] = "Вы уже зарегистрированны - войдите в систему"
