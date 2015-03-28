4ab30535abbcea1cb0637bdbee24f47cf6dff2c6
VMarshinskii <marshinskii@gmail.com>
2015-03-28 20:06:24 +0500

    Update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -171,3 +171,11 @@ LOGGING = {
         },
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'marshinskii@gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'marshinskii@gmail.com'
+EMAIL_HOST_PASSWORD = 'Avagon_VIP5'
+EMAIL_PORT = 587
