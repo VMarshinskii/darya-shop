ce2a021434f6bba3fab9e7f23738bab4a0982446
VMarshinskii <marshinskii@gmail.com>
2015-03-29 21:51:58 +0500

    Update

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -190,9 +190,9 @@ def admin_email(request):
     for rownum in range(sheet.nrows):
         row = sheet.row_values(rownum)
         cl = Clients()
-        cl.name = row[1]
-        cl.surname = row[2]
-        cl.phone = str(row[7]).replace(".0", "")
-        cl.mail = row[3]
+        cl.name = row[1].encode('utf-8')
+        cl.surname = row[2].encode('utf-8')
+        cl.phone = str(row[7]).encode('utf-8').replace(".0", "")
+        cl.mail = row[3].encode('utf-8')
 
     return render_to_response("admin_email.html", args)
