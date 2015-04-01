b788cbe66b9b06676f21567b646532528bd21f19
VMarshinskii <marshinskii@gmail.com>
2015-04-01 23:39:53 +0500

    Update

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -34,14 +34,14 @@ class Address(models.Model):
 
 
 class SiteSettings(models.Model):
-    sine_name = models.CharField(label='Название сайта', max_length=200)
-    description = models.CharField(label='Description', max_length=200)
-    keywords = models.CharField(label='Keywords', max_length=200)
+    sine_name = models.CharField('Название сайта', max_length=200)
+    description = models.CharField('Description', max_length=200)
+    keywords = models.CharField('Keywords', max_length=200)
 
-    phone = models.CharField(label='Телефон', max_length=200)
-    email = models.CharField(label='E-mail', max_length=200)
+    phone = models.CharField('Телефон', max_length=200)
+    email = models.CharField('E-mail', max_length=200)
 
-    vk = models.CharField(label='Вк', max_length=200)
-    inst = models.CharField(label='Instagram', max_length=200)
+    vk = models.CharField('Вк', max_length=200)
+    inst = models.CharField('Instagram', max_length=200)
 
-    head_banner = models.ImageField(verbose_name="Баннер (главный)", upload_to="static/uploads/")
+    head_banner = models.ImageField("Баннер (главный)", upload_to="static/uploads/")
