0f7992bcfede6b304c377bdb06129e6f2ba89378
VMarshinskii <marshinskii@gmail.com>
2015-04-02 16:17:33 +0500

    Update

--- mysite/static/js/admin.js
+++ mysite/static/js/admin.js
@@ -20,10 +20,12 @@ var order_list = function(order_str){
 
 $(document).ready(function(){
     var path = $(".file-upload").children("a").attr("href");
-    $(".file-upload").children("a").attr("href", '/' + path);
-    $(".file-upload").children("a").html('<img width="260px" class="adminImageField" src="/' + path + '"/>');
-    var content = $(".file-upload").html().replace("На данный момент: ", '');
-    $(".file-upload").html(content);
+    if(path){
+        $(".file-upload").children("a").attr("href", '/' + path);
+        $(".file-upload").children("a").html('<img width="260px" class="adminImageField" src="/' + path + '"/>');
+        var content = $(".file-upload").html().replace("На данный момент: ", '');
+        $(".file-upload").html(content);
+    }
 });
 
 
