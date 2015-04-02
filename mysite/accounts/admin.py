88b9bdfa8880f05cf3201559eed2755fa2136d6f
VMarshinskii <marshinskii@gmail.com>
2015-04-02 12:04:50 +0500

    Update

--- mysite/accounts/admin.py
+++ mysite/accounts/admin.py
@@ -1,6 +1,6 @@
 # coding: utf8
 from .forms import AdminUserChangeForm, AdminUserAddForm
-from .models import User, Address
+from .models import User, Address, SiteSettingsForm
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import ugettext_lazy as _
@@ -33,3 +33,4 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(User)
 admin.site.register(Address)
+admin.site.register(SiteSettingsForm)
