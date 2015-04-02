14446a6afe514314d708f7c75c999840d9e1bc08
VMarshinskii <marshinskii@gmail.com>
2015-04-02 15:07:09 +0500

    Update

--- mysite/static/js/admin.js
+++ mysite/static/js/admin.js
@@ -4,4 +4,29 @@ $(document).ready(function(){
     $(".file-upload").children("a").html('<img width="260px" class="adminImageField" src="/' + path + '"/>');
     var content = $(".file-upload").html().replace("На данный момент: ", '');
     $(".file-upload").html(content);
+
+    var order_str = $("#id_products").val();
+    alert(order_list(order_str));
 });
+
+
+//fvpe8pai.jpg;Пальто Green Frash зеленое;1140;4;4560;1
+
+var order_list = function(order_str){
+    var result = "<table>";
+    var products = order_str.split("==");
+    for (var i = 1; i < products.length -1; i++){
+        if(products[i] !== '') {
+            var product = products[i].split(";");
+            result += '<tr>';
+            result += '<td><img width="50" src="' + product[0] + '"/></td>';
+            result += '<td><a href="/catalog/product/' + product[5] + '">' + product[1] + '<a/></td>';
+            result += '<td>' + product[2] + '</td>';
+            result += '<td>' + product[3] + '</td>';
+            result += '<td>' + product[4] + '</td>';
+            result += '</tr>';
+        }
+    }
+    result += '</tr>';
+    return result;
+};
